91167671812d50325fe0ecbf4477173ea3ddba86
antmar <antmar@kth.se>
2017-04-04 14:49:11 +0200

    Fixat så API-anrop funkar

--- js/movieModel.js
+++ js/movieModel.js
@@ -1,4 +1,4 @@
-moviePlannerApp.factory('Movie',function ($resource, $cookies) {
+movieApp.factory('Movie',function ($resource, $cookies) {
 
 
 this.movies = [];
@@ -20,41 +20,54 @@ this.searchMovie = function() {
 
 // Lägg till film i lista
 this.addMovieToList = function(id) {
-    this.movies.push(this.currentMovie;
-    console.log(this.movie);
-    var idString = ""
-    for (element in this.movies) {
-      idString += this.movies[element].id + "_"
-    };
-    console.log(idString);
-    var newString = ""
-    newString = idString.split("_");
-    $cookies.put('movies', newString);
+//    this.movies.push(this.currentMovie;
+//    console.log(this.movie);
+//    var idString = ""
+//    for (element in this.movies) {
+//      idString += this.movies[element].id + "_"
+//    };
+//    console.log(idString);
+//    var newString = ""
+//    newString = idString.split("_");
+//    $cookies.put('movies', newString);
 }
 
 // Tar bort film från listan
 this.removeMovieFromList = function(id) {
-  this.newMovies = [];
-  for (element in this.movies) {
-    var theMovie = this.movies[element];
-    if (theMovie != id) {
-      this.newMovies.push(theMovie);
-    }
-  }
-  this.movies = this.newMovies;
+//  this.newMovies = [];
+//  for (element in this.movies) {
+//    var theMovie = this.movies[element];
+//    if (theMovie != id) {
+//     this.newMovies.push(theMovie);
+//    }
+//  }
+//  this.movies = this.newMovies;
 }
 
 // Betygsätt film
-this.gradeMovie = function() {
+this.MovieSearch = function(query, cb) {
+
+    var api_key = '573bb1edb1c5674d09c84f39d01dcf69';
+
+    $(document).ready(function(){
+      $.ajax({
+        url: 'http://api.themoviedb.org/3/search/movie?api_key=' + api_key + '&query=' + query,
+        dataType: 'jsonp',
+        jsonpCallback: 'testing'
+      }).error(function() {
+        console.log('error')
+      }).done(function(response) {
+          cb(response.results);
+      });
+    });
+  
 
 }
 
-this.MovieSearch = $resource('https://api.themoviedb.org/3/search/movie?api_key=573bb1edb1c5674d09c84f39d01dcf69&query=',{},{
-  get: {
-    headers: {
-      //'X-Mashape-Key': '573bb1edb1c5674d09c84f39d01dcf69'
-    }
-  }
-});
+//this.MovieSearch = $resource('https://api.themoviedb.org/3/search/movie?api_key=573bb1edb1c5674d09c84f39d01dcf69&query=',{},{
+//  get: {}
+//});
+
+return this;
 
 });
--- js/profileCtrl.js
+++ js/profileCtrl.js
@@ -1,2 +1,9 @@
-movieApp.controller('profileCtrl', function ($scope,$routeParams,$cookies) {
-});
+movieApp.controller('profileCtrl', function ($scope,Movie,$cookies) {
+	
+	$scope.search = function(movieQuery) {
+		Movie.MovieSearch(movieQuery, function(results) {
+			console.log(results);
+			$scope.searchResults = results
+			});
+	}
+	});
