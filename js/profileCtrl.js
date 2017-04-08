cd0c028cccb0bdf2be97316e5016327d4ba217fe
olivia-bengtsson <oliviabengtsson@Olivias-MacBook-Pro.local>
2017-04-08 13:59:24 +0200

    ändrat i ctrl

--- js/profileCtrl.js
+++ js/profileCtrl.js
@@ -1,3 +1,10 @@
 movieApp.controller('profileCtrl', function ($scope,Movie,$cookies) {
 
+	//var userId = firebase.auth().currentUser.uid;
+	var userId = 0;
+	return firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
+  		var username = snapshot.val().username;
+  
+});
+
 });
