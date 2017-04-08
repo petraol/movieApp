50b942d7ff856a128d9afa1cd08b57f3425f201f
olivia-bengtsson <oliviabengtsson@Olivias-MacBook-Pro.local>
2017-04-08 13:36:01 +0200

    ändring

--- js/fireBaseService.js
+++ js/fireBaseService.js
@@ -1,7 +1,17 @@
 // Get a reference to the database service
 
-var movieBase = new firebase('https://movieapp-b5887.firebaseio.com/');
-var userId = 0;
+  // Set the configuration for your app
+  // TODO: Replace with your project's config object
+var config = {
+  apiKey: "AIzaSyC2IqIQPw56-1VIJ8F5KqgzG8NdEY1W7BY",
+  authDomain: "movieapp-b5887.firebaseapp.com",
+  databaseURL: "https://movieapp-b5887.firebaseio.com/",
+  storageBucket: "gs://movieapp-b5887.appspot.com"
+  };
+  firebase.initializeApp(config);
+
+  // Get a reference to the database service
+var database = firebase.database();
 
 //function writeUserData(userId, name, sanck, imageUrl) {
 //  firebase.database().ref('users/' + userId).set({
@@ -11,18 +21,12 @@ var userId = 0;
  // });
 //}
 
-// Save data to firebase
-function savedata(userId, name, snack, imageUrl){
-
-  movieBase.child('users').child(userId).push(
-  {
-  	name: name, 
-  	snack: snack,
-  	imageUrl: imageUrl
-
+function writeUserData(userId, name, snack, imageUrl) {
+  firebase.database().ref('users/' + userId).set({
+    username: name,
+    snack: snack,
+    profile_picture : imageUrl
   });
-  
 }
 
-
-savedata(0, "Anton", "Popcorn", "https://scontent-arn2-1.xx.fbcdn.net/v/t31.0-8/15128952_10207573719443039_6551904099240725029_o.jpg?oh=ba077902a4ab5a8b34365a4370102bc6&oe=596A1CA6")
+w'(0, "Anton", "Popcorn", "https://scontent-arn2-1.xx.fbcdn.net/v/t31.0-8/15128952_10207573719443039_6551904099240725029_o.jpg?oh=ba077902a4ab5a8b34365a4370102bc6&oe=596A1CA6")
