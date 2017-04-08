6d9b9578257374ac6d93c3d1b4d4590e8485130a
olivia-bengtsson <oliviabengtsson@Olivias-MacBook-Pro.local>
2017-04-08 13:10:31 +0200

    Ändrat igen i databas

--- js/fireBaseService.js
+++ js/fireBaseService.js
@@ -1,16 +1,28 @@
 // Get a reference to the database service
-var database = firebase.database();
 
-var messageRef = new Firebase('https://movieapp-b5887.firebaseio.com/');
+var movieBase = new Firebase('https://movieapp-b5887.firebaseio.com/');
 var userId = 0;
 
-function writeUserData(userId, name, sanck, imageUrl) {
-  firebase.database().ref('users/' + userId).set({
-    username: name,
-    snack: snack,
-    profile_picture : imageUrl
+//function writeUserData(userId, name, sanck, imageUrl) {
+//  firebase.database().ref('users/' + userId).set({
+ //   username: name,
+    //snack: snack,
+  //  profile_picture : imageUrl
+ // });
+//}
+
+// Save data to firebase
+function savedata(userId, name, snack, imageUrl){
+
+  movieBase.child('users').child(userId).push(
+  {
+  	name: name, 
+  	snack: snack,
+  	imageUrl: imageUrl
+
   });
+  
 }
 
 
-writeUserData(0, "Anton", "Popcorn", "https://scontent-arn2-1.xx.fbcdn.net/v/t31.0-8/15128952_10207573719443039_6551904099240725029_o.jpg?oh=ba077902a4ab5a8b34365a4370102bc6&oe=596A1CA6")
+savedata(0, "Anton", "Popcorn", "https://scontent-arn2-1.xx.fbcdn.net/v/t31.0-8/15128952_10207573719443039_6551904099240725029_o.jpg?oh=ba077902a4ab5a8b34365a4370102bc6&oe=596A1CA6")
