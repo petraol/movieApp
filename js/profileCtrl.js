fb2b7920e33145013c102429a304109c30b3cae7
Petra <petraol@kth.se>
2017-04-08 14:21:33 +0200

    fixat så profilen hämtar från firebase

--- js/profileCtrl.js
+++ js/profileCtrl.js
@@ -1,10 +1,17 @@
 movieApp.controller('profileCtrl', function ($scope,Movie,$cookies) {
-
 	//var userId = firebase.auth().currentUser.uid;
-	var userId = 0;
-	return firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
-  		var username = snapshot.val().username;
-  
-});
-
+		var userId = 0;
+		return firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
+	  		$scope.name = function() {
+	  			return snapshot.val().username;
+	  		}
+	  		$scope.image = function() {
+	  			console.log("bilden", snapshot.val().profile_picture)
+	  			return snapshot.val().profile_picture;
+	  		}
+	  		$scope.snack = function() {
+	  			return  snapshot.val().snack;
+	  		}
+	  
+		});
 });
