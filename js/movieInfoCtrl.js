1cc61a2989d46339f8ee655cc81a485ba205fe20
Petra <petraol@kth.se>
2017-04-10 15:45:02 +0200

    fixat add movie funktion

--- js/movieInfoCtrl.js
+++ js/movieInfoCtrl.js
@@ -1,6 +1,8 @@
 movieApp.controller('movieInfoCtrl', function ($scope,$routeParams,Movie,$cookies) {
 
 	$scope.movieId = Number($routeParams.movieId);
+	//$scope.currentUser = Movie.currentUser;
+	$scope.currentUser = "Anton";
 
 	for (movie in Movie.currentSearch) {
 		//console.log(Movie.currentSearch[movie].id);
@@ -15,10 +17,16 @@ movieApp.controller('movieInfoCtrl', function ($scope,$routeParams,Movie,$cookie
 			var html = "<div id='movieInfo'><h2>" + movie.title + "</h2></br><div class='col-sm-5'><img src='https://image.tmdb.org/t/p/w1280" + movie.poster_path +
 			 "' alt='http://i.imgur.com/SSuPNLC.png' height='600px' width='400px'/></div><div class='col-sm-7' style='font-size: 15pt;'><b>Overview: </b>" + movie.overview +"</br></br><b>Original Language: </b>"
 			 + movie.original_language +"</br></br><b>Average Grade: </b>"+movie.vote_average+"/10 from "+movie.vote_count+
-			 " voters</br></br><div class='col-sm-2'><b><a style='color: #e74c3c;'><img src='http://i.imgur.com/EQWK9jp.png' height='70px' width='70px'/></div><div class='col-sm-10'></br>Lägg till i min lista</b></a></div></div></div>"
+			 " voters</br></br></div>"
 
 			document.getElementById('movieInfo').innerHTML = html 
 		}
+	}
+
+	$scope.add = function() {
+		firebase.database().ref('movieLists/' + $scope.currentUser + "/movie").push().set({
+			movie: $scope.movieId
+		})
 	}
 
 });
