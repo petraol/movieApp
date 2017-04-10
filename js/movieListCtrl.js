838802aab3e513f2dd28cfc79d4247aa8b77dc96
Petra <petraol@kth.se>
2017-04-10 14:21:30 +0200

    ändrat i movielist

--- js/movieListCtrl.js
+++ js/movieListCtrl.js
@@ -1,3 +1,16 @@
 movieApp.controller('movieListCtrl', function ($scope,Movie,$cookies) {
 
+		//var userName = Movie.user;
+		var userName = "Anton"
+		return firebase.database().ref('/movieList/' + userName).on('value', function(snapshot) {
+
+	  		$scope.$apply( function(){
+	  			$scope.list = function() {
+		  			return snapshot.val();
+		  		}
+	  		});
+
+		});
+
+	}
 });
