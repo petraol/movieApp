f981dbc18c2fd446479a776162be16d8098f4292
olivia-bengtsson <oliviabengtsson@n179-p137.kthopen.kth.se>
2017-04-11 13:24:19 +0200

    fixat med users contrl

--- js/friendsCtrl.js
+++ js/friendsCtrl.js
@@ -1,8 +1,24 @@
-movieApp.controller('friendsCtrl', function ($scope,$routeParams,$cookies) {
+movieApp.controller('friendsCtrl', function ($scope,$routeParams,Movie,$cookies) {
 
-	var username = firebase
-	firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
-  	var username = snapshot.val().username;
-  // ...
-});
+$scope.namelist = [];
+$scope.snacklist = [];
+$scope.imagelist = [];
+var username = Movie.currentUser;
+		firebase.database().ref('/users/').on('value', function(snapshot) {
+			snapshot.forEach(function(childSnapshot) {
+				//var user = childSnapshot.child('realname').val()
+				
+				$scope.namelist.push(childSnapshot.child('realname').val())
+				//console.log(childSnapshot.child('realname').val())
+				$scope.snacklist.push(childSnapshot.child('snack').val())
+				//console.log(childSnapshot.child('snack').val())
+				$scope.imagelist.push(childSnapshot.child('profile_picture').val())
+				//console.log(childSnapshot.child('profile_picture').val())
+				//console.log($scope.namelist)
+				//console.log($scope.snacklist)
+				//console.log($scope.imagelist)
+
+			});
+
+		});
 });
