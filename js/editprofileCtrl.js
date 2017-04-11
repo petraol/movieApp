053f26e50a7058ce877a1b5739e0d6a20c063022
antmar <antmar@kth.se>
2017-04-11 16:19:42 +0200

    Nu kan man uppdatera sin profil :D

--- js/editprofileCtrl.js
+++ js/editprofileCtrl.js
@@ -1,3 +1,36 @@
 movieApp.controller('editprofileCtrl', function ($scope,Movie,$cookies,$location,$window) {
 
+	var username = Movie.getCurrentUser();
+	console.log("Vid omladdningen var currentUser: " + username)
+	firebase.database().ref('/users/' + username).on('value', function(snapshot) {
+
+  		$scope.$evalAsync(function() {
+
+  			$scope.name = function() {
+	  			return snapshot.val().realname;
+	  		}
+	  		$scope.image = function() {
+	  			return snapshot.val().profile_picture;
+	  		}
+	  		$scope.snack = function() {
+	  			return snapshot.val().snack;
+	  		}
+  		});
+  	});
+
+  	$scope.edit = function(realname,image,snack) {
+  		console.log(realname);
+  		console.log(image);
+  		console.log(snack);
+  		postEdit = {
+  			realname: realname,
+  			profile_picture: image,
+  			snack: snack
+  		};
+
+  		var updates = {};
+  		updates['/users/' + username] = postEdit;
+  		return firebase.database().ref().update(updates);
+  	}
+
 });
