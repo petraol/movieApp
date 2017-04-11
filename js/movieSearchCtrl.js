1acb95a932004e54e533e2c43c97dbd82ae441ca
antmar <antmar@kth.se>
2017-04-11 21:48:21 +0200

    Karros ändringar?

--- js/movieSearchCtrl.js
+++ js/movieSearchCtrl.js
@@ -1,6 +1,7 @@
 movieApp.controller('movieSearchCtrl', function ($scope,Movie,$cookies) {
-
+$('#loading').hide(); // Hiding loading gif
 	$scope.search = function(movieQuery) {
+		$('#loading').show(); // Showing loading gif
 		Movie.MovieSearch.get({query:movieQuery},
 			function(data) {
 				console.log("Här är data.result");
@@ -12,6 +13,6 @@ movieApp.controller('movieSearchCtrl', function ($scope,Movie,$cookies) {
 			function(data) {
 				console.log("There was an error");
 		});
+		$('#loading').hide(); // Hiding Loading gif. It never show 
 	}
-
 });
