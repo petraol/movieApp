296203e13c7ce09f136ae6f116cbd1bbdde679af
antmar <antmar@kth.se>
2017-04-12 14:35:07 +0200

    Kommer inte ihåg vilka ändringar jag gjorde

--- js/movieSearchCtrl.js
+++ js/movieSearchCtrl.js
@@ -1,7 +1,6 @@
 movieApp.controller('movieSearchCtrl', function ($scope,Movie,$cookies) {
 $('#loading').hide(); // Hiding loading gif
 	$scope.search = function(movieQuery) {
-		$('#loading').show(); // Showing loading gif
 		Movie.MovieSearch.get({query:movieQuery},
 			function(data) {
 				console.log("Här är data.result");
@@ -13,6 +12,6 @@ $('#loading').hide(); // Hiding loading gif
 			function(data) {
 				console.log("There was an error");
 		});
-		$('#loading').hide(); // Hiding Loading gif. It never show 
 	}
+
 });
