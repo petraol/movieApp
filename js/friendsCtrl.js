18edc0059ead4017aa47f2f12cd4c874a6fb74ec
olivia-bengtsson <oliviabengtsson@Olivias-MacBook-Pro.local>
2017-04-12 16:08:44 +0200

    ändrat igen

--- js/friendsCtrl.js
+++ js/friendsCtrl.js
@@ -1,16 +1,17 @@
 movieApp.controller('friendsCtrl', function ($scope,$routeParams,Movie,$cookies) {
 
+//$scope.namelist = [];
+//$scope.snacklist = [];
+//$scope.imagelist = [];
 $scope.list = [];
 var username = Movie.currentUser;
 		firebase.database().ref('/users/').on('value', function(snapshot) {
 			snapshot.forEach(function(childSnapshot) {
+				//var user = childSnapshot.child('realname').val()
 				
-				$scope.namelist.push(childSnapshot).val()
-				
-				console.log((childSnapshot).val())
-				
+				//$scope.namelist.push(childSnapshot.child('realname').val())
+				$scope.list.push(childSnapshot).val()
+
 			});
-
-
 		});
 });
