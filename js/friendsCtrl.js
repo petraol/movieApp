530ba968213f206c67abd683ce989b9d66aab11a
olivia-bengtsson <oliviabengtsson@n188-p136.kthopen.kth.se>
2017-04-18 18:26:24 +0200

    bllll

--- js/friendsCtrl.js
+++ js/friendsCtrl.js
@@ -2,8 +2,12 @@ movieApp.controller('friendsCtrl', function ($scope,$routeParams,Movie,$cookies)
 
 //$scope.namelist = [];
 //$scope.snacklist = [];
-//$scope.imagelist = [];
-$scope.list = [];
+$scope.myFunction2 = function() {
+	    document.getElementById("myDropdown").classList.toggle("show");
+		}
+
+$scope.movielist = [];
+$scope.userlist = [];
 var username = Movie.getCurrentUser();
 console.log(username);
 
@@ -11,16 +15,27 @@ console.log(username);
 		$window.location.assign('#!/oops');
 		console.log(username, "jag är här");
 	}
-	firebase.database().ref('/users/').on('value', function(snapshot) {
 
+	var the_userlist = firebase.database().ref('/users/').on('value', function(snapshot) {
 	snapshot.forEach(function(childSnapshot) {
 				//var user = childSnapshot.child('realname').val()
-
 				//$scope.namelist.push(childSnapshot.child('realname').val())
-	$scope.list.push(childSnapshot.val());
+	$scope.userlist.push(childSnapshot.val());
 				//console.log(childSnapshot.val())
 				//console.log($scope.list)
+				console.log($scope.userlist);
+	});
+	});
+$scope.friendMovies = [];
+this.friendMovieList = function(username) {
+	firebase.database().ref('/movieList/'+username+'/movie').on('value', function(snapshot) {
+	snapshot.forEach(function(childSnapshot) {
+				//var user = childSnapshot.child('realname').val()
+				//$scope.namelist.push(childSnapshot.child('realname').val())
+	$scope.movielist.push(childSnapshot.val());
+	});
+	});
+}
 
-	});
-	});
+
 });
