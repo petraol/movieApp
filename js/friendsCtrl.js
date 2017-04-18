16c5cb610137b53a87bb904bbdab97eebf93ee08
krlnkrlssn <krlnkrlssn@gmail.com>
2017-04-18 18:23:46 +0200

    hatar friends

--- js/friendsCtrl.js
+++ js/friendsCtrl.js
@@ -2,6 +2,10 @@ movieApp.controller('friendsCtrl', function ($scope,$routeParams,Movie,$cookies)
 
 //$scope.namelist = [];
 //$scope.snacklist = [];
+$scope.myFunction2 = function() {
+	    document.getElementById("myDropdown").classList.toggle("show");
+		}
+
 $scope.movielist = [];
 $scope.userlist = [];
 var username = Movie.getCurrentUser();
@@ -12,7 +16,7 @@ console.log(username);
 		console.log(username, "jag är här");
 	}
 
-	firebase.database().ref('/users/').on('value', function(snapshot) {
+	var the_userlist = firebase.database().ref('/users/').on('value', function(snapshot) {
 	snapshot.forEach(function(childSnapshot) {
 				//var user = childSnapshot.child('realname').val()
 				//$scope.namelist.push(childSnapshot.child('realname').val())
@@ -22,19 +26,16 @@ console.log(username);
 				console.log($scope.userlist);
 	});
 	});
-
-	for (user in $scope.userlist) {
-		var theName = $scope.userlist[user].realname
-		firebase.database().ref('/movieList/'+theName+'/movie').on('value', function(snapshot) {
-		snapshot.forEach(function(childSnapshot) {
-					//var user = childSnapshot.child('realname').val()
-					//$scope.namelist.push(childSnapshot.child('realname').val())
-		$scope.movielist.push(childSnapshot.val());
-					//console.log(childSnapshot.val())
-					//console.log($scope.list)
-					console.log($scope.movielist);
+$scope.friendMovies = [];
+this.friendMovieList = function(username) {
+	firebase.database().ref('/movieList/'+username+'/movie').on('value', function(snapshot) {
+	snapshot.forEach(function(childSnapshot) {
+				//var user = childSnapshot.child('realname').val()
+				//$scope.namelist.push(childSnapshot.child('realname').val())
+	$scope.movielist.push(childSnapshot.val());
 	});
-
 	});
-	}
+}
+
+
 });
