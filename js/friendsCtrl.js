042d266b96ebb7ad680f531e115ab4598601f45e
Petra <petraol@kth.se>
2017-04-19 11:24:42 +0200

    fixat med froendslist

--- js/friendsCtrl.js
+++ js/friendsCtrl.js
@@ -1,46 +1,22 @@
 movieApp.controller('friendsCtrl', function ($scope,$routeParams,Movie,$cookies) {
 
-//$scope.namelist = [];
-//$scope.snacklist = [];
-//$scope.myFunction2 = function() {
-//	    document.getElementById("movieDropdown").classList.toggle("show");
-		//}
-
-//$scope.movielist = [];
 $scope.userlist = [];
-var username = Movie.getCurrentUser();
-console.log(username);
+$scope.username = Movie.getCurrentUser();
 
-	if (username === "") {
+
+	if ($scope.username === "") {
 		$window.location.assign('#!/oops');
-		console.log(username, "jag är här");
 	}
 
-	var the_userlist = firebase.database().ref('/users/').on('value', function(snapshot) {
-	snapshot.forEach(function(childSnapshot) {
-				//var user = childSnapshot.child('realname').val()
-				//$scope.namelist.push(childSnapshot.child('realname').val())
-	$scope.userlist.push(childSnapshot.val());
-				//console.log(childSnapshot.val())
-				//console.log($scope.list)
-				//console.log($scope.userlist);
-	});
+	return firebase.database().ref('users/').on('value', function(snapshot) {
+		//$scope.userlist = [];
+		snapshot.forEach(function(childSnapshot) {
+			if (childSnapshot.val().realname !== $scope.username) {
+				$scope.userlist.push(childSnapshot.val());
+			}
+		console.log($scope.userlist)
+				
+		});
 	});	
-
-
-
-
-
-//$scope.friendMovies = [];
-//this.friendMovieList = function(username) {
-//	firebase.database().ref('/movieList/'+username+'/movie').on('value', function(snapshot) {
-//	snapshot.forEach(function(childSnapshot) {
-				//var user = childSnapshot.child('realname').val()
-				//$scope.namelist.push(childSnapshot.child('realname').val())
-//	$scope.movielist.push(childSnapshot.val());
-//	});
-	//});
-//}
-
 
 });
