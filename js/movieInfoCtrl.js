62c5142c0e93070f75f664ddde569f9bd4c19e15
krlnkrlssn <krlnkrlssn@gmail.com>
2017-04-19 22:34:27 +0200

    cleen

--- js/movieInfoCtrl.js
+++ js/movieInfoCtrl.js
@@ -42,7 +42,7 @@ movieApp.controller('movieInfoCtrl', function ($scope,$routeParams,Movie,$cookie
 				}
 				var html = "<div id='movieInfo'><h2>" + data.title + "</h2></br><div class='col-sm-5'><img src='https://image.tmdb.org/t/p/w1280" + data.poster_path +
 				 "' alt='http://i.imgur.com/SSuPNLC.png' height='600px' width='400px'/></div><div class='col-sm-7' style='font-size: 15pt;'><b>Overview: </b>" + data.overview +"</br></br><b>Original Language: </b>"
-				 + language +"</br></br><b>Average Grade: </b>"+data.vote_average+"/10 from "+data.vote_count+
+				 + language +"</br></br><b>Average Rating: </b>"+data.vote_average+"/10 from "+data.vote_count+
 				 " voters</br></br></div>"
 
 				document.getElementById('movieInfo').innerHTML = html 
@@ -66,9 +66,19 @@ movieApp.controller('movieInfoCtrl', function ($scope,$routeParams,Movie,$cookie
 
 			var movie = Movie.currentMovie;
 
+			if (movie.original_language === "en") {
+					language = "English"
+				}
+				else if (movie.original_language === "sv") {
+					language = "Swedish"
+				}
+				else {
+					language = data.original_language;
+				}
+
 			var html = "<div id='movieInfo'><h2>" + movie.title + "</h2></br><div class='col-sm-5'><img src='https://image.tmdb.org/t/p/w1280" + movie.poster_path +
 			 "' alt='http://i.imgur.com/SSuPNLC.png' height='600px' width='400px'/></div><div class='col-sm-7' style='font-size: 15pt;'><b>Overview: </b>" + movie.overview +"</br></br><b>Original Language: </b>"
-			 + movie.original_language +"</br></br><b>Average Rating: </b>"+movie.vote_average+"/10 from "+movie.vote_count+
+			 + language +"</br></br><b>Average Rating: </b>"+movie.vote_average+"/10 from "+movie.vote_count+
 			 " voters</br></br></div>"
 
 			document.getElementById('movieInfo').innerHTML = html 
