6d971bf46e4f39d9fa69f2f3325639d970e12491
antonmedstorta <aejmartinsson@gmail.com>
2017-04-20 00:10:41 +0200

    Update readme.js

--- readme.js
+++ readme.js
@@ -1,8 +1,7 @@
-Git recept:
+HOW TO SETUP: 
 
-git pull (ALLTID det första man gör innan man ändrar en fil!!!)
-git add ..
-git commit -m ””
-git pull
-(git mergetool) om något fel
-git push origin master 
+1. Clone repository.
+2. Since the repository already contains the necessary elements for angular to work, having node installed and 
+running the command 'npm start' in the terminal set up the app to be available at localhost:8000. If 'npm start' doesn't work, 
+a possible solution is to delete the folders node-modules and bower-components, run 'npm install' and then try 'npm start' again.
+3. Go to localhost:8000 in your browser, and the app should run!
