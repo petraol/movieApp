2db1b18c7939680546ccdb4eb2defdaf4f7710ad
antmar <antmar@kth.se>
2017-04-24 16:39:15 +0200

    CreateAccount fixad

--- js/createaccountCtrl.js
+++ js/createaccountCtrl.js
@@ -2,11 +2,13 @@ movieApp.controller('createaccountCtrl', function ($scope,Movie,$cookies,$locati
 
 	$scope.create = function(username, realname, password, snack, imageUrl) {
 
-		if (username == null || password == null || realname == null || snack == null || imageUrl == null) {
-			$("#infoenter").show();
-			$("#usernameerror").hide();
-			return;
-		}
+		console.log("Username: " + username);
+		console.log("Realname: " + realname);
+		console.log("Password: " + password);
+		console.log("Snack: " + snack);
+		console.log("Image: " + imageUrl);
+
+		if (username && realname && password && snack && imageUrl) {
 
 		Movie.setCurrentUser(username);
 
@@ -15,28 +17,58 @@ movieApp.controller('createaccountCtrl', function ($scope,Movie,$cookies,$locati
 				for (value in childSnapshot.W.path.o) {
 
 					if (childSnapshot.W.path.o[value] == "users") {}
-					else {Movie.allUsers += childSnapshot.W.path.o[value];}
-
+					else if (childSnapshot.W.path.o[value] == "undefined") {}
+					else {
+						Movie.allUsers += childSnapshot.W.path.o[value];
+						}
 					}
 				});
 				
 				allUsers = Movie.allUsers;
+				Movie.allUsers = "";
+				console.log(username);
+				console.log(allUsers);
+
+				// if (username || password || realname || snack || imageUrl) {
+				// 	// console.log(password);
+				// 	// console.log(realname);
+				// 	// console.log(snack);
+				// 	// console.log(imageUrl);
+				// 	// console.log(username);
+				// 	console.log("Något fält är en tom sträng");
+				// 	$("#infoenter").show();
+				// 	$("#usernameerror").hide();
+				// }
 
 				if (allUsers.includes(username)) {
+					console.log("Nu stötte den på ett namn som redan finns i databasen")
 					$("#infoenter").hide();
 					$("#usernameerror").show();
+					return;
 				}
 
 				else {
+				
+				try {
 				firebase.database().ref('users/' + username).set({
 					realname: realname,
-				  password: password,
-				  snack: snack,
-				  profile_picture : imageUrl
-				  });
+				  	password: password,
+				  	snack: snack,
+				  	profile_picture : imageUrl
+				  	}); 
 				console.log("Nu har användaren skapats och vi byter view");
 				$window.location.assign('#!/movieSearch');
 				}
-		});
+				catch(err) {
+					console.log("Fel under skapandet i databasen")
+				}
+			}
+			});
+		}
+	else {
+		$("#infoenter").show();
+		$("#usernameerror").hide();
+		return;
+	}
 	}
 });
