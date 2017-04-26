14219c3c1ce8aed96b3f0fa46eeb0a1fb09b4791
krlnkrlssn <krlnkrlssn@gmail.com>
2017-04-26 12:37:57 +0200

    fixat upload image

--- js/editprofileCtrl.js
+++ js/editprofileCtrl.js
@@ -20,35 +20,71 @@ movieApp.controller('editprofileCtrl', function ($scope,Movie,$cookies,$location
 	  		$scope.snack = function() {
 	  			return snapshot.val().snack;
 	  		}
+        $scope.password = function() {
+          return snapshot.val().password;
+        }
   		});
   	});
 
-  	$scope.edit = function(realname,image,snack,pass,pass2) {
-  		console.log(realname);
-  		console.log(image);
-  		console.log(snack);
+  	$scope.passedit = function(pass,pass2) {
+
       console.log(pass);
-      if (pass === pass2) {
-  		postEdit = {
-  			realname: realname,
-  			profile_picture: image,
-  			snack: snack,
-        password: pass
-  		};
+
+    if (pass === undefined || pass2 === undefined) {
+      $("#errorempty").show();
+      $("#errormatch").hide();
+      $("#success").hide();
+    }
+
+    else if (pass === pass2) {
+
       $("#errorempty").hide();
       $("#errormatch").hide();
       $("#success").show();
 
-  		var updates = {};
-  		updates['/users/' + username] = postEdit;
-  		return firebase.database().ref().update(updates);
+      newData = {
+        realname: $scope.name(),
+        profile_picture: $scope.image(),
+        snack: $scope.snack(),
+        password: pass
+      }
+
+      var update = {};
+      update['/users/' + username] = newData;
+      return firebase.database().ref().update(update);
   	}
-    else if (pass === undefined || pass2 === undefined) {
-      $("#errorempty").show();
-    }
     else if (pass != pass2) {
       $("#errormatch").show();
+      $("#success").hide();
+      $("#errorempty").hide();
     }
+    }
+
+    $scope.edit = function(realname, image, snack) {
+
+      if (typeof realname != 'string') {
+        realname = $scope.name();
+      }
+      
+      if (typeof image != 'string') {
+        image = $scope.image();
+      }
+
+      if (typeof snack != 'string') {
+        snack = $scope.snack();
+      }
+
+      newData = {
+        realname: realname,
+        profile_picture: image,
+        snack: snack,
+        password: $scope.password()
+      }
+
+      var update = {};
+      update['/users/' + username] = newData;
+      console.log('Updated!')
+      return firebase.database().ref().update(update);
     }
 
 });
