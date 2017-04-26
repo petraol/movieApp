b863f88f9019e7b20b09454fdeeb80c1cf2d8bc9
olivia-bengtsson <oliviabengtsson@n186-p227.kthopen.kth.se>
2017-04-26 10:52:45 +0200

    felhantering

--- js/movieSearchCtrl.js
+++ js/movieSearchCtrl.js
@@ -2,8 +2,24 @@ movieApp.controller('movieSearchCtrl', function ($scope,Movie,$cookies) {
 $('#loading').hide(); // Hiding loading gif
 	$scope.search = function(movieQuery) {
 		Movie.MovieSearch.get({query:movieQuery},
+			
 			function(data) {
-				$scope.searchResults = data.results;
+
+
+				$scope.searchPosterResults = data.results;
+				console.log(data.results)
+				for (poster in data.results.post_path) {
+					console.log(poster)
+					if($scope.searchPosterResults === undefined) {
+						$scope.searchPosterResults = "http://www.dimaria.dk/images/feed/product/filmrulle_s-p.jpg"
+					} 
+					else {
+						$scope.searchPosterResults = data.results;
+	   				 // Object is NOT empty
+					}
+				}
+				
+				//$scope.searchResults = data.results;
 				$scope.currentUser = Movie.getCurrentUser();
 				Movie.currentSearch = data.results;
 				Movie.removeCurrentMovie();
