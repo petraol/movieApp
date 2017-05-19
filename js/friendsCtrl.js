7667170a5e4117e925c5c780c9baac679520c338
krlnkrlssn <krlnkrlssn@gmail.com>
2017-05-19 11:49:44 +0200

    halvfärdig friends

--- js/friendsCtrl.js
+++ js/friendsCtrl.js
@@ -12,9 +12,42 @@ movieApp.controller('friendsCtrl', function ($scope,$routeParams,Movie,$cookies)
 			snapshot.forEach(function(childSnapshot) {
 				if (childSnapshot.val().realname !== $scope.username) {
 					$scope.userlist.push(childSnapshot.val());
-				}
-			});
-		});
-	});
 
+					var pic;
+										$scope.image = function() {
+											console.log("Vi är i image");
+											var profileRef = firebase.database().ref("users/" + childSnapshot.key);
+											profileRef.child("profile_picture").once('value', function(snapshot) {
+												pic = snapshot.val();
+												//var path = snapshot.fullPath;
+												//console.log(path);
+											});
+											console.log(pic);
+											var storage = firebase.storage().ref();
+											var spaceRef = storage.child('images/' + pic);
+											console.log(spaceRef);
+											var path = spaceRef.fullPath;
+											console.log(path);
+										//	console.log(profileRef.val());
+											//var profileRef = root.(username.profile_picture);
+
+													storage.child(path).getDownloadURL().then(function(url){
+														var image_url = url;
+														console.log(image_url);
+														document.querySelector("#friend_pic").src = image_url;
+
+													}).catch(function(error) {
+													console.log("error")
+													console.log(error.code);
+													console.log(error.message);
+													});
+											}
+
+				//	$scope.userlist.push();
+
+			}
+		});
+
+});
+});
 });
