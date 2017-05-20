e8d3f87be6056362553dcc373132eb55d725838e
antmar <antmar@kth.se>
2017-05-20 11:28:43 +0200

    På väg mot en fungerande otherusers

--- js/friendsCtrl.js
+++ js/friendsCtrl.js
@@ -11,41 +11,39 @@ movieApp.controller('friendsCtrl', function ($scope,$routeParams,Movie,$cookies)
 		$scope.$evalAsync(function() {
 			snapshot.forEach(function(childSnapshot) {
 				if (childSnapshot.val().realname !== $scope.username) {
-					$scope.userlist.push(childSnapshot.val());
+					$scope.userlist.push(childSnapshot);
 
 					var pic;
-										$scope.image = function() {
-											console.log("Vi är i image");
-											var profileRef = firebase.database().ref("users/" + childSnapshot.key);
-											profileRef.child("profile_picture").once('value', function(snapshot) {
-												pic = snapshot.val();
-												//var path = snapshot.fullPath;
-												//console.log(path);
-											});
-											console.log(pic);
-											var storage = firebase.storage().ref();
-											var spaceRef = storage.child('images/' + pic);
-											console.log(spaceRef);
-											var path = spaceRef.fullPath;
-											console.log(path);
-										//	console.log(profileRef.val());
-											//var profileRef = root.(username.profile_picture);
 
-													storage.child(path).getDownloadURL().then(function(url){
-														var image_url = url;
-														console.log(image_url);
-														document.querySelector("#friend_pic").src = image_url;
+							$scope.image = function() {
+								for (user in $scope.userlist) {
+								var profileRef = firebase.database().ref("users/" + $scope.userlist[user].key);
+								profileRef.child("profile_picture").once('value', function(snapshot) {
+									pic = snapshot.val();
 
-													}).catch(function(error) {
-													console.log("error")
-													console.log(error.code);
-													console.log(error.message);
-													});
-											}
+		
+								});
+								var storage = firebase.storage().ref();
+								var spaceRef = storage.child('images/' + pic);
+								console.log('Här är spaceRef: ' + spaceRef);
+								var path = spaceRef.fullPath;
+								console.log("Här är path: " + path);
+							//	console.log(profileRef.val());
+								//var profileRef = root.(username.profile_picture);
 
-				//	$scope.userlist.push();
+								storage.child(path).getDownloadURL().then(function(url){
+									var image_url = url;
+									console.log("Här är imageURL: " + image_url);
+									document.querySelector("#friend_pic").src = image_url;
 
-			}
+								}).catch(function(error) {
+								console.log("error")
+								console.log(error.code);
+								console.log(error.message);
+								});
+						}
+					}
+				}
 		});
 
 });
