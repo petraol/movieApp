c024160745a28ed6de3ebbe1b5ef19f5f38f6a4d
antmar <antmar@kth.se>
2017-05-29 17:57:28 +0200

    Call it a day

--- js/profileCtrl.js
+++ js/profileCtrl.js
@@ -1,6 +1,7 @@
 movieApp.controller('profileCtrl', function ($scope,Movie,$cookies,$location,$window) {
 	//var userId = firebase.auth().currentUser.uid;
 		var username = Movie.getCurrentUser();
+		$scope.src = "";
 
 		if (username == "") {
 			$window.location.assign('#!/oops');
@@ -8,7 +9,6 @@ movieApp.controller('profileCtrl', function ($scope,Movie,$cookies,$location,$wi
 
 		if ( window.location.href === "http://127.0.0.1:8000/#!/movieList") {
 			$("findme").hide();
-
 		}
 
 		console.log("Vid omladdningen var currentUser: " + username)
@@ -16,40 +16,32 @@ movieApp.controller('profileCtrl', function ($scope,Movie,$cookies,$location,$wi
 
 	  		$scope.$evalAsync(function() {
 
+				var pic;
+				var profileRef = firebase.database().ref("users/" + username);
+				profileRef.child("profile_picture").once('value', function(snapshot) {
+					pic = snapshot.val();
+
+				});
+				var storage = firebase.storage().ref();
+				var spaceRef = storage.child('images/' + pic);
+				var path = spaceRef.fullPath;
+
+						storage.child(path).getDownloadURL().then(function(url){
+							var image_url = url;
+							console.log(image_url);
+							$scope.$apply($scope.src = image_url);
+
+						}).catch(function(error) {
+						console.log("error")
+						console.log(error.code);
+						console.log(error.message);
+						});
+
+
 	  			$scope.name = function() {
 		  			return snapshot.val().realname;
 		  		}
-var pic;
-					$scope.image = function() {
-						console.log(username);
-						console.log("Vi är i image");
-						var profileRef = firebase.database().ref("users/" + username);
-						profileRef.child("profile_picture").once('value', function(snapshot) {
-							pic = snapshot.val();
-							//var path = snapshot.fullPath;
-							//console.log(path);
-						});
-						console.log(pic);
-						var storage = firebase.storage().ref();
-						var spaceRef = storage.child('images/' + pic);
-						console.log(spaceRef);
-						var path = spaceRef.fullPath;
-						console.log(path);
-
-								storage.child(path).getDownloadURL().then(function(url){
-									var image_url = url;
-									console.log(image_url);
-									document.querySelector("#profilepic").src = image_url;
-
-								}).catch(function(error) {
-								console.log("error")
-								console.log(error.code);
-								console.log(error.message);
-								});
-
-						}
-
-					$scope.snack = function() {
+				$scope.snack = function() {
 		  			return snapshot.val().snack;
 		  		}
 
@@ -57,18 +49,3 @@ var pic;
 
 		});
 });
-
-
-
-/*
-		  		$scope.image = function() {
-		  			var storage = firebase.storage();
-						var pathReference = storage.ref('images/');
-						var varurl = pathReference.child('Branschdagenporträtt-2016liten.png').getDownloadURL().then(function(url) {
-							dostuff(url)
-							});
-						var dostuff = function(url) {
-							console.log(url);
-							$scope.image = url;
-						}
-		  		} */
