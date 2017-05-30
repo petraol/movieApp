f626579f42966d97801076845186f1290154c8e8
antmar <antmar@kth.se>
2017-05-30 21:36:30 +0200

    Fixat profilbilder

--- js/profileCtrl.js
+++ js/profileCtrl.js
@@ -1,14 +1,14 @@
 movieApp.controller('profileCtrl', function ($scope,Movie,$cookies,$location,$window) {
+	//var userId = firebase.auth().currentUser.uid;
 		var username = Movie.getCurrentUser();
 		$scope.src = "";
-		
+
 		if (username == "") {
 			$window.location.assign('#!/oops');
 		}
 
 		if ( window.location.href === "http://127.0.0.1:8000/#!/movieList") {
 			$("findme").hide();
-
 		}
 
 		console.log("Vid omladdningen var currentUser: " + username)
@@ -16,34 +16,32 @@ movieApp.controller('profileCtrl', function ($scope,Movie,$cookies,$location,$wi
 
 	  		$scope.$evalAsync(function() {
 
+				var pic;
+				var profileRef = firebase.database().ref("users/" + username);
+				profileRef.child("profile_picture").once('value', function(snapshot) {
+					pic = snapshot.val();
+
+				});
+				var storage = firebase.storage().ref();
+				var spaceRef = storage.child('images/' + pic);
+				var path = spaceRef.fullPath;
+
+						storage.child(path).getDownloadURL().then(function(url){
+							var image_url = url;
+							console.log(image_url);
+							$scope.$apply($scope.src = image_url);
+
+						}).catch(function(error) {
+						console.log("error")
+						console.log(error.code);
+						console.log(error.message);
+						});
+
+
 	  			$scope.name = function() {
 		  			return snapshot.val().realname;
 		  		}
-				var pic;
-					$scope.image = function() {
-						var profileRef = firebase.database().ref("users/" + username);
-						profileRef.child("profile_picture").once('value', function(snapshot) {
-							pic = snapshot.val();
-							//var path = snapshot.fullPath;
-							//console.log(path);
-						});
-						var storage = firebase.storage().ref();
-						var spaceRef = storage.child('images/' + pic);
-						var path = spaceRef.fullPath;
-
-							storage.child(path).getDownloadURL().then(function(url){
-								var image_url = url;
-								$scope.$apply($scope.src = image_url);
-
-								}).catch(function(error) {
-								console.log("error")
-								console.log(error.code);
-								console.log(error.message);
-								});
-
-						}
-
-					$scope.snack = function() {
+				$scope.snack = function() {
 		  			return snapshot.val().snack;
 		  		}
 
@@ -51,18 +49,3 @@ movieApp.controller('profileCtrl', function ($scope,Movie,$cookies,$location,$wi
 
 		});
 });
-
-
-
-/*
-		  		$scope.image = function() {
-		  			var storage = firebase.storage();
-						var pathReference = storage.ref('images/');
-						var varurl = pathReference.child('Branschdagenporträtt-2016liten.png').getDownloadURL().then(function(url) {
-							dostuff(url)
-							});
-						var dostuff = function(url) {
-							console.log(url);
-							$scope.image = url;
-						}
-		  		} */
